19d171b5db4b460d416e0f6a160123c671855197
Tilak Puli <tilakpuli15@gmail.com>
2023-09-12 09:31:42 +0530

    Update primary pallete color

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,6 +3,11 @@ import { createTheme } from "@mui/material/styles";
 import { orange } from "@mui/material/colors";
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#175274",
+    },
+  },
   status: {
     danger: orange[500],
   },
